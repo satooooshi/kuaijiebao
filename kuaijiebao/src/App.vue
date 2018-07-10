2bea1cdf0407a8d96bc026ab66e779db34f524b0
Howard <978118325@qq.com>
2018-07-10 16:23:05 +0800

    更改导航栏布局，由横向改为纵向

--- kuaijiebao/src/App.vue
+++ kuaijiebao/src/App.vue
@@ -2,89 +2,107 @@
   <div>
 
   <div id ="app">
-    <div>这是{{count}}</div>
-    <el-button >count+</el-button>
-    <el-menu :default-active="activeIndex2"
-  class="el-menu-demo"
-  mode="horizontal"
-  @select="handleSelect"
-  background-color="#545c64"
-  text-color="#fff"
-  active-text-color="#ffd04b">
-  <el-menu-item index="1" @click.native = "to0">首页</el-menu-item>
-  <el-submenu index="2">
-    <template slot="title">我要借款</template>
-    <el-menu-item index="2-1">借款申请</el-menu-item>
-    <el-menu-item index="2-2">借款管理</el-menu-item>
-  </el-submenu>
-  <el-submenu index="3">
-    <template slot="title">我要理财</template>
-    <el-menu-item index="3-1">理财产品管理</el-menu-item>
-    <el-menu-item index="3-2">我的债权</el-menu-item>
-    <el-menu-item index="3-3">债权转让</el-menu-item>
-  </el-submenu>
-  <el-submenu index="4">
-    <template slot="title">信用管理</template>
-    <el-menu-item index="4-1">信用查询</el-menu-item>
-    <el-menu-item index="4-2">信用额度管理</el-menu-item>
-  </el-submenu>
-  <el-submenu index="5">
-    <template slot="title">数据统计</template>
-    <el-menu-item index="5-1">支出统计</el-menu-item>
-    <el-menu-item index="5-2">借款统计</el-menu-item>
-    <el-menu-item index="5-3">理财统计</el-menu-item>
-  </el-submenu>
-  <el-submenu index="6">
-    <template slot="title">我的账户</template>
-    <el-menu-item index="6-1">个人信息管理</el-menu-item>
-    <el-menu-item index="6-2">个人安全管理</el-menu-item>
-    <el-menu-item index="6-3">银行卡管理</el-menu-item>
-    <el-menu-item index="6-4">退出登录</el-menu-item>
-  </el-submenu>
-  <el-submenu index="7">
-    <template slot="title">咨询</template>
-    <el-menu-item index="4-1">常见问题</el-menu-item>
-    <el-menu-item index="4-2">在线咨询</el-menu-item>
-  </el-submenu>
-  <el-submenu index="8" disabled>
-    <template slot="title" >后台管理</template>
-    <el-menu-item index="4-1">用户管理</el-menu-item>
-    <el-menu-item index="4-2">理财产品管理</el-menu-item>
-  </el-submenu>
-  <el-menu-item index="9">登录/注册</el-menu-item>
+    <el-row class="tac">
+      <el-col :span="12">
+        <h4>快借宝</h4>
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose">
 
-</el-menu>
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-star-on"></i>
+              <span>我要借款</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">借款申请</el-menu-item>
+              <el-menu-item index="1-2">借款管理</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
 
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-star-on"></i>
+              <span>我要理财</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="2-1">理财产品管理</el-menu-item>
+              <el-menu-item index="2-2">我的债权</el-menu-item>
+              <el-menu-item index="2-3">债权转让</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-star-on"></i>
+              <span>信用管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="3-1">信用查询</el-menu-item>
+              <el-menu-item index="3-2">信用额度管理</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-star-on"></i>
+              <span>咨询</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="4-1">常见问题咨询</el-menu-item>
+              <el-menu-item index="4-2">在线咨询</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-star-on"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="5-1">借还款统计</el-menu-item>
+              <el-menu-item index="5-2">理财统计</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+
+          <el-submenu index="6">
+            <template slot="title">
+              <i class="el-icon-star-on"></i>
+              <span>个人中心</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="6-1">个人信息管理</el-menu-item>
+              <el-menu-item index="6-2">个人安全管理</el-menu-item>
+              <el-menu-item index="6-3">银行卡管理</el-menu-item>
+              <el-menu-item index="6-4">退出登录</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+
+        </el-menu>
+      </el-col>
+    </el-row>
   </div>
   </div>
 </template>
 
 <script>
 export default {
-  data: function () {
-    return {
-      activeIndex: '1',
-      activeIndex2: '1',
-      pos: 3,
-      count: 1
-    }
-  },
   methods: {
-    to0: function () {
-      count ++
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
     }
   }
-
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
 </style>
