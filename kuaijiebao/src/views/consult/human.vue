7ce8f650fd056266969b460d58aad72b075bc9bc
Howard <978118325@qq.com>
2018-07-24 23:43:25 +0800

    前端所有组件开发完成

---
+++ kuaijiebao/src/views/consult/human.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>人工客服在线为您服务！</p>
+  <el-button type="text">客服1</el-button>
+  <el-button type="text">客服2</el-button>
+  <el-button type="text">客服3</el-button>
+  <el-button type="text">客服4</el-button>
+  <el-button type="text">客服5</el-button>
+  <el-button type="text">客服6</el-button>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "human"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ kuaijiebao/src/views/credit/manage.vue
@@ -0,0 +1,13 @@
+<template>
+<p>对不起，该功能暂未开放</p>
+</template>
+
+<script>
+    export default {
+        name: "manage"
+    }
+</script>
+
+<style scoped>
+
+</style>
